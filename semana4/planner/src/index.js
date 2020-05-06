00f7905858a466af3d8401b37b4428331430cf50
Renata Mitsue <rmkarato@gmail.com>
2020-05-05 21:12:36 -0300

    completando exercicios

--- semana4/planner/src/index.js
+++ semana4/planner/src/index.js
@@ -1,18 +1,18 @@
-const inputNovaTarefa = document.getElementById("novaTarefa")
+const inputNovaTarefa = document.getElementById("novaTarefa") //input nova tarefa
 
-const minhaTarefa = document.getElementsByTagName("ul")[0]
+const minhaTarefa = document.getElementsByTagName("ul")[0] //pegando a ul do html
 
 function adicionaTarefa () {
-    let tarefa = inputNovaTarefa.value
-    let escolherDia = document.getElementById("diaDaSemana").value   
+    let tarefa = inputNovaTarefa.value //input da tarefa
+    let escolherDia = document.getElementById("diaDaSemana").value //seleção de dia da semana 
     
-    if (inputNovaTarefa != "") {
-        document.getElementById(escolherDia).innerHTML += `<li>${tarefa}</li>`;
-    } else if (inputNovaTarefa = "") {
+    if (inputNovaTarefa.length < 1) {
         alert(`Vocë deve escrever alguma coisa!`)
+    } else  {
+        document.getElementById(escolherDia).innerHTML          += `<li>${tarefa}</li>`
     }
     inputNovaTarefa.value = ""
 }
 
-minhaTarefa.innerHTML //conecta com a ul do html
- 
+ //conecta com a ul do html
+ minhaTarefa.innerHTML
