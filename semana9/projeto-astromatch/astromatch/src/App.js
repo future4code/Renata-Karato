dbeaef8a632cb850993865423025c32fc578c983
Renata Mitsue <rmkarato@gmail.com>
2020-06-16 11:38:17 -0300

    inicio do projeto

---
+++ semana9/projeto-astromatch/astromatch/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+const AppContainer = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+export default function App() {
+  return (
+    <AppContainer>
+        <p>Teste</p>
+    </AppContainer>
+  );
+}
